985ab13668e73f06c5525ba2a4c879b949a6495a
Shinobi Works <support@shinobiworks.com>
2020-04-22 15:23:48 +0900

    database

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,7 +2,9 @@
 
 namespace Shinobi_Works\WP;
 
-class Bootstrap extends DB {
+use Shinobi_Works\WP\DB;
+
+class Bootstrap {
 
 	public static function init() {
 		add_action( 'init', [ __CLASS__, 'create_options_table' ] );
@@ -15,14 +17,14 @@ class Bootstrap extends DB {
 	 */
 	public static function create_options_table() {
 		$version = '1.0.1';
-		$table   = parent::OPTIONS_TABLE;
+		$table   = DB::OPTIONS_TABLE;
 		$sql     = '
 		ID bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
 		option_name varchar(191) NOT NULL,
 		option_value longtext NOT NULL,
 		PRIMARY KEY  id (ID)
 		';
-		parent::create_table( $version, $table, $sql );
+		DB::create_table( $version, $table, $sql );
 	}
 
 }
